d24bb9073f0d43a5f139a95c83d1010d7cfc8966
Kenji Akiyama <artifactsauce@gmail.com>
2018-12-01 03:23:50 +0900

    no need to change mod

--- brew/default.sh
+++ brew/default.sh
@@ -7,6 +7,5 @@ mpkg.install.do_before() {
 }
 
 mpkg.install.do_after() {
-    sudo chown root:wheel /usr/local/Cellar/htop-osx/*/bin/htop
-    sudo chmod u+s /usr/local/Cellar/htop-osx/*/bin/htop
+    :
 }
