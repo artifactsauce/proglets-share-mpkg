39ef4cb1d65a78837e45bac5657c667264556111
Kenji Akiyama <artifactsauce@gmail.com>
2018-12-21 20:41:28 +0900

    set shibang

--- brew/default.sh
+++ brew/default.sh
@@ -1,11 +1,13 @@
+#!/usr/bin/env bash
+
 # setup-pkg-brew
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 mpkg.install.do_before() {
-    :
+  :
 }
 
 mpkg.install.do_after() {
-    :
+  :
 }
--- brew/hobby.sh
+++ brew/hobby.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
